4b309c3a900dbfcba019901899f5c855986305fa
Pavel Trofimov <baygulf@yandex.ru>
2021-12-06 21:29:27 +0800

    Второй

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int f = 1;
         System.out.println("Начало программы");
+        System.out.println("Середина программы");
     }
 }
